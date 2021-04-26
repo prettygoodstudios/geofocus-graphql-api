c5b5be979236b9384eb3e4c6ba0be61f40b73f04
Miguel Rust <hava12soft@gmail.com>
2021-04-26 16:47:30 -0600

    Implemented some additional validation logic

--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -1,6 +1,7 @@
-import { Connection, In } from "typeorm";
+import { ApolloError } from "apollo-server-errors";
 import User, { UserRepository } from "../models/user";
 import { PublicSlugResolver, StandardResolver } from "../types";
+import { AuthError } from "./auth";
 
 export const topUsers: StandardResolver<Promise<User[]>> = async (parent, args, {orm})  => {    
 
@@ -28,17 +29,21 @@ export const user: PublicSlugResolver<Promise<User>> = async (parent, {slug}, {o
     return user!;
 }
 
-export const me: StandardResolver<Promise<User|undefined>> = async (parent, args, {orm, req}) => {
-    const user: User|undefined = await orm
-        .manager 
-        .connection 
-        .getRepository(User)
-        .findOne({
-            where: {
-                id: req.userId
-            },
-            relations: ["photos", "photos.location", "photos.user"]
-        });
-
-    return user;
+export const me: StandardResolver<Promise<User|null>> = async (parent, args, {orm, req}) => {
+    try {
+        const user: User|undefined = await orm
+            .manager 
+            .connection 
+            .getRepository(User)
+            .findOneOrFail({
+                where: {
+                    id: req.userId
+                },
+                relations: ["photos", "photos.location", "photos.user"]
+            });
+            return user;
+    }catch{
+        AuthError("You must be authenticated to perform this action.");
+    }
+    return null;
 }
