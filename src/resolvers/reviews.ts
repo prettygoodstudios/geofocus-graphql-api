2c7becd55970d101bc3c87af7f87983962b4a3d9
Miguel Rust <hava12soft@gmail.com>
2021-06-23 21:50:30 -0600

    Added error handling

--- src/resolvers/reviews.ts
+++ src/resolvers/reviews.ts
@@ -5,16 +5,19 @@ import { PublicSlugResolver } from "../types";
 
 
 export const reviews: PublicSlugResolver<Promise<Review[]>> = async (parent, {slug}, {orm, req}) => {
-    const location = await orm
-        .manager 
-        .connection 
-        .getRepository(Location)
-        .findOneOrFail({
-            relations: ["reviews", "reviews.location", "reviews.user"],
-            where: {
-                slug
-            }
-        });
-    
-    return location.reviews;
+    try {
+        const location = await orm
+            .manager 
+            .connection 
+            .getRepository(Location)
+            .findOneOrFail({
+                relations: ["reviews", "reviews.location", "reviews.user"],
+                where: {
+                    slug
+                }
+            });
+            return location.reviews;
+    } catch {
+        return [];
+    }
 }
